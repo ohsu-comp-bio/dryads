01e638a78206c8388d604960bb94d96081cb99e1
Michal Grzadkowski <mgrzad@gmail.com>
2018-07-26 13:34:00 -0700

    Adding basic cohorts, integration tests, Travis CI files, and package setup.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='dryad',
+      version='0.1',
+      description='Prediction Cancer Phenotypes Using Mutation Dryads',
+      author='Michal Radoslaw Grzadkowski',
+      author_email='grzadkow@ohsu.edu',
+      package_dir={'dryad': ''},
+      packages=['dryad'],
+     )
+
---
+++ tests/test_cohorts.py
@@ -0,0 +1,28 @@
+
+import os
+base_dir = os.path.dirname(__file__)
+
+import sys
+sys.path.extend([os.path.join(base_dir, '../..')])
+
+from dryad.features.cohorts.base import UniCohort
+import pandas as pd
+
+
+def main():
+    expr_data = pd.read_csv("dryad/tests/resources/expr.txt.gz",
+                            sep='\t', index_col=0)
+
+    cdata = UniCohort(expr_data,
+                      train_samps=list(expr_data.index[:120]),
+                      test_samps=list(expr_data.index[120:]))
+
+    assert (cdata.train_samps | cdata.test_samps) == set(expr_data.index), (
+        "Cohort training and testing samples should be a partition "
+        "of the original samples!"
+        )
+
+
+if __name__ == '__main__':
+    main()
+
